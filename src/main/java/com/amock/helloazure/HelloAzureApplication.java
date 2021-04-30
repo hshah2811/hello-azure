c648a0f573b003c7636dd013b696f5bb8967b513
hshah2811 <hshah2811@gmail.com>
2021-04-30 19:41:09 +0530

    Update HelloAzureApplication.java
    webhook event added.

--- src/main/java/com/amock/helloazure/HelloAzureApplication.java
+++ src/main/java/com/amock/helloazure/HelloAzureApplication.java
@@ -1,9 +1,14 @@
 package com.amock.helloazure;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @SpringBootApplication
 public class HelloAzureApplication {
@@ -18,6 +23,18 @@ public class HelloAzureApplication {
 		@GetMapping
 		public String home(){
 			return "index";
+		}
+	}
+	
+	@RestController
+	class WebhookController
+	{
+		Logger logger = LoggerFactory.getLogger(WebhookController.class);
+		@PostMapping("/webhook")
+		public String webhook(@RequestBody String event)
+		{
+			logger.info("event {}",event);
+			return "1";
 		}
 	}
 }
